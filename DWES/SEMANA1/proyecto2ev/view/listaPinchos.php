a990a5f75552c47c6ec13c2eab20061f73a33559
HugoRicoCuello <hugoricocuello@gmail.com>
2022-02-12 16:22:56 +0100

    entrega Backend 14-02-2022

--- DWES/SEMANA1/proyecto2ev/view/listaPinchos.php
+++ DWES/SEMANA1/proyecto2ev/view/listaPinchos.php
@@ -12,12 +12,12 @@
             NUEVO
         </button>
     </div>
-    <table class="table table-hover table-bordered">
+    <table class="table table-hover table-bordered" id="tablaPinchos">
         <thead>
             <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Bar</th>
+                <th scope="col" id="0" onclick="ordenaTabla(0,'int')">ID</th>
+                <th scope="col" id="1" onclick="ordenaTabla(1,'str')">Nombre</th>
+                <th scope="col" id="2" onclick="ordenaTabla(2,'int')">Bar</th>
             </tr>
         </thead>
         <tbody id="myTable">
@@ -133,5 +133,62 @@
         let id = tr.getElementsByClassName("id")[0].innerHTML;
 
         window.location = "http://localhost/DWES/SEMANA1/proyecto2ev/index.php/fichaPincho?nombre=" + nombre + "&bar=" + bar + "&id=" + id;
+    }
+
+    /**
+     * Funcion para ordenar una tabla... tiene que recibir el numero de columna a
+     * ordenar y el tipo de orden
+     * @param int id_columna
+     * @param str orden - ['str'|'int']
+     */
+    function ordenaTabla(id_columna, orden) {
+        var tabla, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+        tabla = document.getElementById("tablaPinchos");
+        switching = true;
+        //Set the sorting direction to ascending:
+        dir = "asc";
+
+        /*Make a loop that will continue until no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = tabla.rows;
+            /*Loop through all table rows (except the first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare, one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("td")[id_columna];
+                y = rows[i + 1].getElementsByTagName("td")[id_columna];
+                /*check if the two rows should switch place, based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                    if ((orden == "str" && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) || (orden == "int" && parseFloat(x.innerHTML) > parseFloat(y.innerHTML))) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if ((orden == "str" && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) || (orden == "int" && parseFloat(x.innerHTML) < parseFloat(y.innerHTML))) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount++;
+            } else {
+                /*If no switching has been done AND the direction is "asc", set the direction to "desc" and run the while loop again.*/
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
     }
 </script>
--- DWES/SEMANA1/proyecto2ev/view/listaUsuarios.php
+++ DWES/SEMANA1/proyecto2ev/view/listaUsuarios.php
@@ -12,13 +12,13 @@
             NUEVO
         </button>
     </div>
-    <table class="table table-hover table-bordered">
+    <table class="table table-hover table-bordered" id="tablaUsuarios">
         <thead>
             <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Email</th>
-                <th scope="col">Password</th>
-                <th scope="col">Admin</th>
+                <th scope="col" id="0" onclick="ordenaTabla(0,'int')">ID</th>
+                <th scope="col" id="1" onclick="ordenaTabla(1,'str')">Email</th>
+                <th scope="col" id="2" onclick="ordenaTabla(2,'str')">Password</th>
+                <th scope="col" id="3" onclick="ordenaTabla(3,'int')">Admin</th>
             </tr>
         </thead>
         <tbody id="myTable">
@@ -125,5 +125,62 @@
         let id = tr.getElementsByClassName("id")[0].innerHTML;
 
         window.location = "http://localhost/DWES/SEMANA1/proyecto2ev/index.php/fichaUsuario?email=" + email + "&pwd=" + pwd + "&admin=" + admin + "&id=" + id;
+    }
+
+    /**
+     * Funcion para ordenar una tabla... tiene que recibir el numero de columna a
+     * ordenar y el tipo de orden
+     * @param int id_columna
+     * @param str orden - ['str'|'int']
+     */
+    function ordenaTabla(id_columna, orden) {
+        var tabla, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+        tabla = document.getElementById("tablaUsuarios");
+        switching = true;
+        //Set the sorting direction to ascending:
+        dir = "asc";
+
+        /*Make a loop that will continue until no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = tabla.rows;
+            /*Loop through all table rows (except the first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare, one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("td")[id_columna];
+                y = rows[i + 1].getElementsByTagName("td")[id_columna];
+                /*check if the two rows should switch place, based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                    if ((orden == "str" && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) || (orden == "int" && parseFloat(x.innerHTML) > parseFloat(y.innerHTML))) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if ((orden == "str" && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) || (orden == "int" && parseFloat(x.innerHTML) < parseFloat(y.innerHTML))) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount++;
+            } else {
+                /*If no switching has been done AND the direction is "asc", set the direction to "desc" and run the while loop again.*/
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
     }
 </script>
