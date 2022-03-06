22b5e7f6b853946df98943e37fcc30eaeeb8f862
HugoRicoCuello <hugoricocuello@gmail.com>
2022-03-06 21:35:00 +0100

    Update logrocho.sql

--- DWES/logrocho.sql
+++ DWES/logrocho.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-03-2022 a las 21:30:08
+-- Tiempo de generación: 06-03-2022 a las 21:34:24
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
